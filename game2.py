e75a2d1d49c392eab67bb9339737464d43bb0f73
asad632 <74180557+asad632@users.noreply.github.com>
2020-12-01 21:30:23 +0400

    game

---
+++ game2.py
@@ -0,0 +1,18 @@
+# import random
+# wining_number = random.randint(1,100)
+# guess = 1
+# number = int(input("enter number b/w 1 - 100 ; "))
+# game_over = False
+#
+# while True:
+#     if number == wining_number:
+#         print(f"you win in {guess} times")
+#         game_over = True
+#         break
+#     else:
+#         if number > wining_number:
+#             print(" too high ")
+#         else:
+#             print("too low")
+#         guess += 1
+#         number=int(input("try agin "))
