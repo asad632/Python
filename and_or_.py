68e257ee2aa469bad745aad6e0482e074234c9fd
asad632 <74180557+asad632@users.noreply.github.com>
2020-12-06 15:45:49 +0400

    Create and_or_.py

---
+++ and_or_.py
@@ -0,0 +1,16 @@
+# # and , or conditions use for to check two conditions at same time
+# # and condion : if both conditions are true then it'll consider true but
+# # one or both condions are fales it will consider fales.
+# # or conditon : if both or one condion is true its true but if both are fale
+# # it will consider fale
+# a = 13
+# b = 10
+# if a ==13 and b ==10: # == will use instead =
+#     print('True condition')
+# else:
+#     print('Fale condition')
+#
+# if a ==13 or b ==10:
+#     print('True condition')
+# else:
+#     print('Fale condition')
