66b1bf4912b197916257635fda5739bcc164e86b
asad632 <74180557+asad632@users.noreply.github.com>
2020-12-06 15:45:03 +0400

    Add files via upload

---
+++ and_exercise.py
@@ -0,0 +1,7 @@
+# name = input("name ")
+# age = input("age ")
+# age=int(age)
+# if age >= 10 and (name[0]=='a' or name[0]=='A'):
+#     print('welcome')
+# else:
+#     print('you can not watch coco')
